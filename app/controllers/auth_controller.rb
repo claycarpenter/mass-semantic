b770e4d61a6101c4b67d0d7728f8b00db7f42f24
Clay Carpenter <claycarpenter@gmail.com>
2015-09-15 17:15:06 -0400

    Adds sign in page and basic OAuth functionality links.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,11 @@
 class AuthController < ApplicationController
   def sign_in
+    # TODO: These really should be part of the app configuration, not
+    # dynamically generated for each request.
+    @urls = {
+      :github => "https://github.com/login/oauth/authorize?scope=user:email&client_id=#{ ENV['github_client_id']}",
+      :stackex => "https://stackexchange.com/oauth?client_id=#{ENV['stackex_client_id']}&scope=&redirect_uri=http://localhost:3000/se_auth/callback"
+    }
   end
 
   def sign_up
