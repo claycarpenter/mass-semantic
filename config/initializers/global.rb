1effeb7158643071ee751cf2b0634d6ab28da834
Clay Carpenter <claycarpenter@gmail.com>
2015-09-17 09:18:30 -0400

    Adds load hook for Global config file parser.

---
+++ config/initializers/global.rb
@@ -0,0 +1,5 @@
+# Load configuration files held in the config/global directory.
+Global.configure do |config|
+  config.environment = Rails.env.to_s
+  config.config_directory = Rails.root.join('config/global').to_s
+end
