2376326ba2207f527f9327fabd0a5f668318e16f
Clay Carpenter <claycarpenter@gmail.com>
2015-09-19 17:05:28 -0400

    Adds indices for User fields email, username to ensure uniqueness.

---
+++ db/migrate/20150919210428_add_indices_to_users_username_email.rb
@@ -0,0 +1,6 @@
+class AddIndicesToUsersUsernameEmail < ActiveRecord::Migration
+  def change
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919195027) do
+ActiveRecord::Schema.define(version: 20150919210428) do
 
   create_table "users", force: :cascade do |t|
     t.string   "display_name"
@@ -22,5 +22,8 @@ ActiveRecord::Schema.define(version: 20150919195027) do
     t.datetime "updated_at",   null: false
     t.string   "username"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
