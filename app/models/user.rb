76ecf2135334f5d739880bd1c149467ed24b6983
Clay Carpenter <claycarpenter@gmail.com>
2015-09-19 16:44:51 -0400

    Adds validation to the username, emails fields of User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_USERNAME_REGEX = /\A[a-z0-9]{1}[\w]+[a-z0-9]{1}\z/i
+
+  validates :username,
+    presence: true,
+    length: {maximum: 25},
+    format: {with: VALID_USERNAME_REGEX},
+    uniqueness: {case_sensitive: false}
+
+  validates :email,
+    presence: true,
+    length: {maximum: 250},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
 end
