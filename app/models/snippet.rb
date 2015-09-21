3414eba8a32d75675b90d70f179d0c50a0034b62
Clay Carpenter <claycarpenter@gmail.com>
2015-09-21 15:57:04 -0400

    Adds validations to Snippet properties.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,20 @@
 class Snippet < ActiveRecord::Base
+  # Relations
   belongs_to :user
+
+  # Validations
+  validates :title,
+    presence: true,
+    length: {minimum: 5, maximum: 150}
+
+  validates :code,
+    presence: true,
+    length: {minimum: 10}
+
+  validates :expl_md,
+    presence: true,
+    length: {minimum: 5}
+
+  validates :user_id,
+    presence: true
 end
