0a2677f74b5a76619cea7dc4afc8f9410dd0630a
Clay Carpenter <claycarpenter@gmail.com>
2015-09-21 19:57:30 -0400

    Adds basic comment add form and create support.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+    @snippet = Snippet.find(params[:snippet_id])
+    @comment = @snippet.comments.create(comment_params)
+    @comment.user = current_user
+    @comment.save
+
+    redirect_to snippet_path(@snippet)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
