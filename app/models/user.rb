700210a253d4cbe05034cd7ff6833d36b8bab101
Clay Carpenter <claycarpenter@gmail.com>
2015-09-22 18:59:56 -0400

    Initial Devise installation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, :registerable,
+         :trackable, :validatable, :confirmable
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_USERNAME_REGEX = /\A[a-z0-9]{1}[\w]+[a-z0-9]{1}\z/i
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :snippets do
     resources :comments, shallow: true
   end
