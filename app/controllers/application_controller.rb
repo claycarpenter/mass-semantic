5af27e1812a0299dc820fbff8299d6b2535851b1
Clay Carpenter <claycarpenter@gmail.com>
2015-09-23 18:01:15 -0400

    Allows email, username, and display_name parameters on Devise User models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # TODO: Not sure if these two helpers are necessary...
   def new_session_path(scope)
     new_user_session_path
@@ -10,5 +12,12 @@ class ApplicationController < ActionController::Base
 
   def session_path(scope)
     user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    logger.debug "Configuring allowing sign up parameters (permitted)"
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:display_name, :email, :username)}
   end
 end
