e22179b72403b4a5c93ee80082b497847a1a1dc8
Clay Carpenter <claycarpenter@gmail.com>
2015-09-26 16:17:52 -0400

    Adds comments#create support for JSON formats.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,15 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     @comment.save
 
-    redirect_to snippet_path(@snippet)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to snippet_path(@snippet) }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /comments/1
@@ -36,6 +44,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body_md, :snippet_id)
     end
 end
