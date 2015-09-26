e49e79e807fd449e464a5fc88f94c3470de92da3
Clay Carpenter <claycarpenter@gmail.com>
2015-09-26 18:09:48 -0400

    Adds comments#destroy functionality.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
 
+  # POST /snippets/1/comments
+  # POST /snippets/1/comments.json
   def create
     @snippet = Snippet.find(params[:snippet_id])
     @comment = @snippet.comments.create(comment_params)
@@ -21,6 +23,20 @@ class CommentsController < ApplicationController
   # GET /comments/1
   # GET /comments/1.json
   def show
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to snippet_path(@comment.snippet) }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
