5eb81d28aae3b7b00dd06a7952a10c9d88500397
Clay Carpenter <claycarpenter@gmail.com>
2015-09-26 16:58:48 -0400

    Adds support for logging the current user along with each request.
    Logging is conditional based on environment variable/config value.
    
    Updates application.yml to include new security debug log config value.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :log_user, if: :security_debug_log?
 
   # TODO: Not sure if these two helpers are necessary...
   def new_session_path(scope)
@@ -19,5 +20,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     logger.debug "Configuring allowing sign up parameters (permitted)"
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:display_name, :email, :username)}
+  end
+
+  def security_debug_log?
+    Global.application.security.debug_log
+  end
+
+  def log_user
+    Rails.logger.debug "Current user: #{current_user.inspect}"
   end
 end
