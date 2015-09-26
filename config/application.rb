ff173d2462b00f074e4696f90efcf7ad357028ef
Clay Carpenter <claycarpenter@gmail.com>
2015-09-26 18:37:35 -0400

    Configures application to raise an excpetion if unpermitted parameters are submitted.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module MassSemantic
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Throw an error when unpermitted paramters are submitted.
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
